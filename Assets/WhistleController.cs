03d96952c81c8ab1c7b7b2a213aca2b0f6436b0f
James Noonan <james.n1640@gmail.com>
2026-03-07 15:47:03 +1000

    feat: animate whistle on click

--- Assets/WhistleController.cs
+++ Assets/WhistleController.cs
@@ -4,8 +4,18 @@ using UnityEngine.InputSystem;
 public class WhistleController : MonoBehaviour
 {
     public Camera cam;
-    public Vector3 offset;
+    public Vector3 offset = new Vector3(0.0f, 1.5f, 0.0f);
     public float maxDistance = 100f;
+
+    public Vector3 activeScale = new Vector3(1.0f, 1.0f, 1.0f);
+    public Quaternion activeRotation;
+    public float rotationSpeed = 1.0f;
+    public float scaleSpeed = 1.0f;
+
+
+    private Vector3 originalScale;
+    private Quaternion originalRotation;
+    private bool isPressed = false;
 
     void Start()
     {
@@ -13,12 +23,31 @@ public class WhistleController : MonoBehaviour
         {
             cam = Camera.main;
         }
+
+        originalScale = transform.localScale;
+        originalRotation = transform.rotation;
     }
 
     void Update()
     {
         Vector3 groundPosition = ShootRayFromMousePosition();
         transform.position = groundPosition + offset;
+
+        isPressed = Mouse.current.leftButton.isPressed;
+
+        Vector3 targetScale = isPressed ? activeScale : originalScale;
+        Quaternion targetRotation = isPressed ? activeRotation : originalRotation;
+
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            targetRotation,
+            Time.deltaTime * rotationSpeed
+        );
+        transform.localScale = Vector3.MoveTowards(
+            transform.localScale,
+            targetScale,
+            Time.deltaTime * scaleSpeed
+        );
     }
 
     Vector3 ShootRayFromMousePosition()
