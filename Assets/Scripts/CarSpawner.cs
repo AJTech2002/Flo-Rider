0f7741ea6fa49b9a491f5a2252579b829d8a4157
James Noonan <james.n1640@gmail.com>
2026-03-08 13:40:33 +1000

    feat: car movement and spawn cars

---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    public float spawnInterval = 0.5f; 
+    
+    void Start()
+    {
+        if (prefabToSpawn != null)
+            StartCoroutine(SpawnRoutine());
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            SpawnPrefab();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    void SpawnPrefab()
+    {
+        int xRandom = Random.Range(-6, 6) * 2;
+        int zRandom = Random.Range(-6, 6) * 2;
+
+        if (xRandom == 0 && zRandom == 0) return; // Tower
+        
+        Vector3 spawnPosition = new Vector3(
+            xRandom,
+            0,
+            zRandom
+        );
+        
+        
+
+        Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+    }
+}
